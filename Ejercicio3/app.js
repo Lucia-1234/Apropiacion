631cd182af6c53152d1d093a779a6870f4269ac3
Lucia <Lulizcano.aa@hotmail.com>
2025-12-02 15:44:35 -0500

    Se finalizo el ejercicio 3 y se agrego lo solicitado por el instructor

---
+++ Ejercicio3/app.js
@@ -0,0 +1,11 @@
+// Definimos las tres notas
+let nota1 = 10;   // Primera nota
+let nota2 = 50;   // Segunda nota
+let nota3 = 20;   // Tercera nota
+
+// Calculamos el promedio
+// Fórmula: (nota1 + nota2 + nota3) / cantidad de notas
+let promedio = (nota1 + nota2 + nota3) / 3;
+
+// Mostramos el resultado en consola
+console.log("El promedio de las tres notas es: " + promedio);
